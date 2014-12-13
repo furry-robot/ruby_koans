79ee1e6e3b114eab60cdac05dcbb1c3610374309
furry-robot <furry.robot@outlook.com>
2014-12-13 15:54:31 -0500

    complete

--- about_objects.rb
+++ about_objects.rb
@@ -36,8 +36,7 @@ class AboutObjects < Neo::Koan
     assert_equal 5, 2.object_id
     assert_equal 201, 100.object_id
 
-    # THINK ABOUT IT:
-    # What pattern do the object IDs for small integers follow?
+    # THINK ABOUT IT: What pattern do the object IDs for small integers follow?
   end
 
   def test_clone_creates_a_different_object
